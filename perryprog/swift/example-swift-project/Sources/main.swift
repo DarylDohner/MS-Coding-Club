524cf8b7757a3c0f13b5675d9f1a795072613b44
Perry Fraser <perry@frasers.org>
2016-10-07 16:12:51 -0400

    Made da code do some stuff.

--- perryprog/swift/example-swift-project/Sources/main.swift
+++ perryprog/swift/example-swift-project/Sources/main.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+func fibonacci(number: Int) -> (Int) {
+    if number <= 1 {
+        return number
+    } else {
+        return fibonacci(number: number - 1) + fibonacci(number: number - 2)
+    }
+}
+
+var fibNumber = fibonacci(number: 10)
+print("The 10th Fibonacci number is: \(fibNumber)")
